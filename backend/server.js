b9656a4e605f25e92e0a085ed57881585747440b
Srijan Sahu <srijansahu564@gmail.com>
2026-04-05 23:02:11 +0530

    In server.js frontend url connect fix

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ socketHandler(io);
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cors({
-    origin: 'https://re-loop-two.vercel.app',
+    origin: 'https://re-loop-to059ndm5-srijan-sahus-projects.vercel.app',
     credentials: true
 }))
 
